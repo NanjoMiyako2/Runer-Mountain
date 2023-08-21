18b5c060fa511c777970204883dc252af829e177
NanjoMiyako2 <140793758+NanjoMiyako2@users.noreply.github.com>
2023-08-21 16:14:18 +0900

    Add files via upload

--- RunnerMountain.js
+++ RunnerMountain.js
@@ -2,8 +2,17 @@ function test() {
     navigator.geolocation.getCurrentPosition(test2)
 }
 
+
+var firstFlg = true;
+
+var prev_lat = 0;
+var prev_long = 0;
+
 function test2(position) {
 
+	lat2 = position.coords.latitude
+	long2 = position.coords.longitude
+	
     var geo_text = "緯度:" + position.coords.latitude + "\n";
     geo_text += "経度:" + position.coords.longitude + "\n";
     geo_text += "高度:" + position.coords.altitude + "\n";
@@ -16,8 +25,70 @@ function test2(position) {
 
     geo_text += "取得時刻:" + date.toLocaleString() + "\n";
 
+
+    var dist1 = 0;
+    if(firstFlg != true){
+    	dist1 = distance(prev_lat, prev_long, lat2, long2)
+    	
+    }else{
+    	firstFlg = false;
+    }
+    prev_lat = lat2;
+    prev_long = long2;
+    
+    
+    
+    geo_text += "移動距離:" + dist1 + "メートル \n";
     alert(geo_text);
     
     setTimeout("test()", 60000)
 
+}
+
+/**
+ * ２地点間の距離(m)を求める
+ * ヒュベニの公式から求めるバージョン
+ *
+ * @param float $lat1 緯度１
+ * @param float $lon1 経度１
+ * @param float $lat2 緯度２
+ * @param float $lon2 経度２
+ * @param boolean $mode 測地系 true:世界(default) false:日本
+ * @return float 距離(m)
+ */
+function distance($lat1, $lon1, $lat2, $lon2, $mode=true)
+{
+    // 緯度経度をラジアンに変換
+    $radLat1 = deg2rad($lat1); // 緯度１
+    $radLon1 = deg2rad($lon1); // 経度１
+    $radLat2 = deg2rad($lat2); // 緯度２
+    $radLon2 = deg2rad($lon2); // 経度２
+
+    // 緯度差
+    $radLatDiff = $radLat1 - $radLat2;
+
+    // 経度差算
+    $radLonDiff = $radLon1 - $radLon2;
+
+    // 平均緯度
+    $radLatAve = ($radLat1 + $radLat2) / 2.0;
+
+    // 測地系による値の違い
+    $a = $mode ? 6378137.0 : 6377397.155; // 赤道半径
+    $b = $mode ? 6356752.314140356 : 6356078.963; // 極半径
+    //$e2 = ($a*$a - $b*$b) / ($a*$a);
+    $e2 = $mode ? 0.00669438002301188 : 0.00667436061028297; // 第一離心率^2
+    //$a1e2 = $a * (1 - $e2);
+    $a1e2 = $mode ? 6335439.32708317 : 6334832.10663254; // 赤道上の子午線曲率半径
+
+    $sinLat = Math.sin($radLatAve);
+    $W2 = 1.0 - $e2 * ($sinLat*$sinLat);
+    $M = $a1e2 / (Math.sqrt($W2)*$W2); // 子午線曲率半径M
+    $N = $a / Math.sqrt($W2); // 卯酉線曲率半径
+
+    $t1 = $M * $radLatDiff;
+    $t2 = $N * Math.cos($radLatAve) * $radLonDiff;
+    $dist = Math.sqrt(($t1*$t1) + ($t2*$t2));
+
+    return $dist;
 }
