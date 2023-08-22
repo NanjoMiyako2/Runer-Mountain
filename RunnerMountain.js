7f0b648702b495d08741b56d446649ea93a421ef
NanjoMiyako2 <140793758+NanjoMiyako2@users.noreply.github.com>
2023-08-22 17:21:51 +0900

    Add files via upload

--- RunnerMountain.js
+++ RunnerMountain.js
@@ -1,3 +1,192 @@
+
+var firstFlg = true;
+
+var prev_lat = 0;
+var prev_lon = 0;
+
+var runPt = 0;
+var keisokuFlg = false;
+
+if(localStorage.getItem('runPt') != null){
+	runPt = Number(localStorage.getItem('runPt'));
+}
+
+runPt = 900;
+
+var allHanaDict = {}
+
+allHanaDict["アイ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%a4%ef%bc%88%e3%82%bf%e3%83%87%e3%82%a2%e3%82%a4%ef%bc%89/"
+allHanaDict["アイスランドポピー"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%a4%e3%82%b9%e3%83%a9%e3%83%b3%e3%83%89%e3%83%9d%e3%83%94%e3%83%bc%ef%bc%88%e3%82%b7%e3%83%99%e3%83%aa%e3%82%a2%e3%83%92%e3%83%8a%e3%82%b2%e3%82%b7%ef%bc%89/";
+allHanaDict["アグロステンマ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%b0%e3%83%ad%e3%82%b9%e3%83%86%e3%83%b3%e3%83%9e%ef%bc%88%e3%83%a0%e3%82%ae%e3%83%8a%e3%83%87%e3%82%b7%e3%82%b3%ef%bc%89/";
+allHanaDict["アイイロニワゼキショウ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%a4%e3%82%a4%e3%83%ad%e3%83%8b%e3%83%af%e3%82%bc%e3%82%ad%e3%82%b7%e3%83%a7%e3%82%a6/";
+allHanaDict["アイズシモツケ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%a4%e3%82%ba%e3%82%b7%e3%83%a2%e3%83%84%e3%82%b1/";
+allHanaDict["アオキ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%aa%e3%82%ad/";
+allHanaDict["アオギリ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%aa%e3%82%ae%e3%83%aa/";
+allHanaDict["アオチドリ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%aa%e3%83%81%e3%83%89%e3%83%aa%ef%bc%88%e3%83%8d%e3%83%a0%e3%83%ad%e3%83%81%e3%83%89%e3%83%aa%ef%bc%89/";
+allHanaDict["アオテンナンショウ"] = "https://minhana.net/wiki/%e3%82%a2%e3%82%aa%e3%83%86%e3%83%b3%e3%83%8a%e3%83%b3%e3%82%b7%e3%83%a7%e3%82%a6/";
+
+var detectedHanaDict = {}
+detectedHanaDict["アイ"] = true;
+detectedHanaDict["アイスランドポピー"] = false;
+detectedHanaDict["アグロステンマ"] = true;
+detectedHanaDict["アイイロニワゼキショウ"] = false; 
+detectedHanaDict["アイズシモツケ"] = false; 
+detectedHanaDict["アオキ"] = false; 
+detectedHanaDict["アオギリ"] = false; 
+detectedHanaDict["アオチドリ"] = false; 
+detectedHanaDict["アオテンナンショウ"] = false;
+
+
+var MountainHanaDict = {}
+MountainHanaDict["A山"] = ["アイ", "アイスランドポピー", "アグロステンマ"]
+MountainHanaDict["B山"] = ["アイイロニワゼキショウ", "アイズシモツケ", "アオキ"]
+MountainHanaDict["C山"] = ["アオギリ", "アオチドリ", "アオテンナンショウ"]
+
+
+if(localStorage.getItem('saveData') != null){
+	detectedHanaDict = JSON.parse(localStorage.getItem('saveData'));
+	
+}
+
+
+function UseRunPt(){
+
+	if(runPt < 30){
+		return;
+	}
+
+	runPt = runPt - 30;
+	MountainName = document.getElementById("MountainNames").value;
+	
+	HanaList = MountainHanaDict[MountainName];
+	hana1 = HanaList[Math.floor(Math.random()*HanaList.length)];
+	detectedHanaDict[hana1] = true;
+	localStorage.setItem('saveData', JSON.stringify(detectedHanaDict));
+	
+	
+	spanElem = document.getElementById("detecteHana1");
+	spanElem.innerHTML = hana1
+	
+	writeRunPt();
+	
+}
+function countDetectedHana(){
+
+	ret = 0;
+	
+	keys = Object.keys(detectedHanaDict);
+	for(key of keys){
+		if(detectedHanaDict[key] == true){
+			ret++;
+		}
+	}
+	
+	return ret;
+}
+
+function showHanaZukan(){
+
+	hanaZukanH2 = document.getElementById("hanaZukan1");
+
+	str1 = ""
+	
+	detectHanaCount = countDetectedHana();
+	elem1 = document.getElementById("detectedHanaCount1")
+	elem1.innerHTML = detectHanaCount
+	
+	keys = Object.keys(allHanaDict);
+	keys.sort();
+	
+	for(key of keys){
+		if(detectedHanaDict[key] == true){
+			str1 +=  "<a href=\"" + allHanaDict[key] + "\">" + key +"</a><br>"
+		}else{
+			str1 += "？<br>"
+		}
+	}
+	
+	hanaZukanH2.innerHTML = str1
+	
+	
+}
+
+
+
+function changeTab(link){
+  var id = link.hash.slice(1);
+  var page = document.getElementById(id);
+
+    current.page.style.display = 'none';
+    if(current.menu != null){
+	    current.menu.className = '';
+	}
+    page.style.display = 'block';
+    link.className = 'active';
+    current.page = page;
+    current.menu = link;
+    
+}
+
+var current; // 現在のタブの状態を保持する変数
+
+//タブのリンク一覧
+var Tablinks = [];
+
+
+(function(){
+var menu = document.getElementById('tab_menu1');
+var content = document.getElementById('tab_content1');
+var links = menu.getElementsByTagName('a');
+
+
+//Tablinkのリスト取得
+for (var i = 0, l = links.length;i < l; i++){
+  if(links[i].className == 'Tablink'){
+  	Tablinks.push(links[i]);
+  }
+}
+//各タブの初期化
+for (var j = 0;j < Tablinks.length; j++){
+  tab_init(Tablinks[j], j);
+}
+function tab_init(link, index){
+  var id = link.hash.slice(1);
+  var page = document.getElementById(id);
+  if (!current){ // 状態の初期化
+    current = {page:page, menu:link};
+    page.style.display = 'block';
+    link.className = 'active';
+  } else {
+    page.style.display = 'none';
+  }
+  
+  
+  if(id == 'pageUseRunPt'){//ランポイント使用タブの初期表示メソッド
+  	  link.onclick = function(){
+  	  	changeTab(link);
+  	  	
+		writeRunPt();
+		
+  	  	
+  	  	return false;
+
+  	  }
+  }else if(id == 'pageHanaZukan'){//花図鑑タブの初期表示メソッド
+	  link.onclick = function(){
+		  	changeTab(link);
+
+			showHanaZukan();
+			
+			return false;
+		};
+  }
+  
+}
+})();
+
+
+
+
 function keisoku() {
     navigator.geolocation.getCurrentPosition(test2);
 }
@@ -12,18 +201,8 @@ function stopKeisoku(){
 }
 
 
-var firstFlg = true;
 
-var prev_lat = 0;
-var prev_lon = 0;
 
-var runPt = 0;
-var keisokuFlg = false;
-
-if(localStorage.getItem('runPt') != null){
-	runPt = Number(localStorage.getItem('runPt'));
-}
-writeRunPt()
 
 function writeRunPt(){
 	span1 = document.getElementById("RunPt1");
